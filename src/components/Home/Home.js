08a2967b1bf3f98cb34f5897b68a4031b747e7f7
Maksim Pyzhin <maksimkarybaev@gmail.com>
2021-04-16 11:45:01 +0600

    added image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import "./Home.css"
 
 const Home = () => {
     return (
 
   <div className="Home">
-             <h1>Home </h1>
+             <h1>Best battle royale games
+             </h1>
+             <img alt="YouTube Thumbnail" width="100%" class=" lazyloaded" data-src="https://img.youtube.com/vi/w3YZsHEgx5o/sddefault.jpg" src="https://img.youtube.com/vi/w3YZsHEgx5o/sddefault.jpg"></img>
+             <p>The origins of battle royale games are manifold, but what they involve is largely the same: lots of players go in, only one leaves. Battle royale is a violent, unpredictable, and frenzied multiplayer mode where the only goal is to endure until you’re the sole survivor.
+
+All tactics and styles of play are equally viable in the best battle royale games. You can win by hiding in bushes and waiting for the herd to thin out, or scavenge a weapon and start reducing the numbers yourself. With the explosion of Fortnite, PUBG, and Apex Legends, you would be forgiven for thinking there are only three battle royales out there, but there are now numerous other contenders to challenge them, each with its own spin on the established formula. And with player counts fluctuating all over the place, it’s important to know the most popular battle royale games so you can be sure to get a match.
+
+Whether you’re after games like PUBG or an inventive new twist on the formula, there is a battle royale game out there for everyone. So, to help you broaden your horizons, we have assembled this handy guide to the best battle royale games on PC.
+
+</p>
+
+<h2>CRSED: F.O.A.D,
+Call of Duty: Warzone,
+Hyper Scape,
+Apex Legends,
+PlayerUnknown's Battlegrounds,
+Fortnite,
+Firestorm,
+Dying Light: Bad Blood,
+ZombsRoyale.io,
+Ring of Elysium,
+Realm Royale,
+Totally Accurate Battlegrounds,
+Darwin Project,
+Z1 Battle Royale,
+Unturned: Arena Mode</h2>
+<img class="alignnone wp-image-156128 size-large lazyloaded" title="best-battle-royale-games-cuisine-royale (1)" alt="Play Cuisine Royale for free" width="900" height="506" sizes="(max-width: 900px) 100vw, 900px" data-src="https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-900x506.jpg" data-srcset="https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-900x506.jpg 900w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-213x120.jpg 213w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-740x416.jpg 740w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-768x432.jpg 768w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-580x326.jpg 580w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-297x167.jpg 297w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-183x103.jpg 183w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-280x158.jpg 280w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-263x148.jpg 263w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-233x132.jpg 233w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-124x70.jpg 124w" srcset="https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-900x506.jpg 900w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-213x120.jpg 213w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-740x416.jpg 740w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-768x432.jpg 768w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-580x326.jpg 580w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-297x167.jpg 297w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-183x103.jpg 183w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-280x158.jpg 280w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-263x148.jpg 263w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-233x132.jpg 233w, https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-124x70.jpg 124w" src="https://www.pcgamesn.com/wp-content/uploads/2020/02/best-battle-royale-games-cuisine-royale-1-900x506.jpg"></img>
         </div>
     )
 };
--- src/components/PUBG/PUBG.js
+++ src/components/PUBG/PUBG.js
@@ -7,27 +7,27 @@ const PUBG = () => {
   <div className={classes.PUBG}>
  
  <h1>Playerunknown’s Battlegrounds</h1>
-        <p>В 2017-ом году рейтинги, которые представляли
-самые кассовые игры с режимом battle royale возглавила игра PlayerUnknown’s Battlegrounds.
-Сокращенным названием игры, под которой она и стала широко известна геймерам,
-стала аббревиатура PUBG. Разработчиком данного продукта стала компания PUBG Corporation.
-Сам игровой процесс базируется сразу на нескольких дополнениях для других игр,
-разработкой которых занимался небезызвестный Брендан Грин под своим рабочим псевдонимом 
-PlayerUnknown. В итоге получилась вполне самостоятельная игра, которая быстро привлекла
-внимание массовой аудитории. В игровом процессе задействуют до сотни игроков, которые 
-при помощи парашютов десантируются на островок, на котором пытаются найти оружие и
-оснастку для того, чтобы уничтожить своих противников и при этом обязательно выжить.
-На игровом пространстве будет доступна и так называемая безопасная зона, на территории
-орой можно перевести дух. Тем не менее, со временем это пространство начинает уменьшаться,
-чтобы пользователи быстрее вступали в схватку друг с другом. Последний 
-уцелевший геймер или же объединившийся альянс и будут объявлены победителями 
-раунда. В целом игра получила более чем положительные отзывы геймеров.
-Отмечается широкий выбор доступного оружия, внушительные возможности
-проявить своего персонажа в драке, неплохая графика. Несмотря на то,
-что игра уже давненько доступна в полноценном режиме, геймеры сетуют 
-и на внушительное количество игровых багов. Впрочем, такие недочеты не
-помешали PlayerUnknown’s Battlegrounds обогнать большинство своих соперников и 
-получить титул самой кассовой и самой массовой игры своего жанра.</p>
+        <p>In 2017, the ratings that represented
+The most cash games with the Battle Royale regimen headed the Playerunknown's Battlegrounds game.
+Abbreviated name of the game, under which it has become widely known to the gamers,
+Pubg abbreviation has become.  The developer of this product was Pubg Corporation.
+The gameplay itself is based on several additions for other games,
+The development of which was engaged in the notorious Brendan Green under his working pseudonym
+Playerunknown.  As a result, it turned out a very independent game that quickly attracted
+The attention of the mass audience.  In the game process, use up to hundreds of players who
+With the help of parachutes, landing on the island, which are trying to find a weapon and
+Snap to destroy your opponents and at the same time be sure to survive.
+The so-called safe zone will be available on the game space, on the territory
+Oroa can be transferred to the Spirit.  Nevertheless, over time, this space begins to decrease,
+So that users faster entered into a fight with each other.  Last
+surviving gamer or the same united alliance and will be announced by winners
+Round.  In general, the game received more than positive reviews of gamers.
+There is a wide selection of available weapons, impressive opportunities.
+Show your character in a fight, good graphics.  Despite,
+that the game is already long available in full-fledged mode, gamers complain
+And on the impressive number of gaming bugs.  However, such shortcomings are not
+prevented Playerunknown's Battlegrounds to overtake most of their rivals and
+Get the title of the registraly and the most popular game of your genre.</p>
         </div>
       
         
